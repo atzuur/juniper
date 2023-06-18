a3a2a0b29d57d5648c547d990f311a48a8b7dca6
june <136140456+june550@users.noreply.github.com>
2023-06-18 00:12:06 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
+import logging
 
+import disnake
+from disnake.ext import commands
+
+from config import TOKEN
+
+
+# the recommended logging setup 
+# https://docs.disnake.dev/en/stable/logging.html
+logger = logging.getLogger("disnake")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="disnake.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+logger.addHandler(handler)
+
+
+bot = commands.InteractionBot(
+    intents=disnake.Intents.all(),
+    test_guilds=[1115670885702041640],
+    activity=disnake.Game(name="With Code")
+)
+
+@bot.event
+async def on_ready():
+    print("The bot is online!")
+
+bot.run(TOKEN)
