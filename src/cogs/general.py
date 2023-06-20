8bd7b11907bb9b2e89fe7c99c89c5ee5f816dc9e
june <136140456+june550@users.noreply.github.com>
2023-06-19 22:23:19 -0400

    Update general.py

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -1,6 +1,8 @@
 import disnake
 from disnake.ext import commands
 
+from config import SUCCESS
+
 
 class GeneralCommands(commands.Cog):
 
@@ -11,6 +13,19 @@ class GeneralCommands(commands.Cog):
     async def ping(self, inter: disnake.AppCmdInter):
         """Get the bot's current websocket latency"""
         await inter.send(f"Pong! {round(self.bot.latency * 1000)}ms")
+
+
+    @commands.slash_command()
+    async def avatar(inter: disnake.AppCmdInter, user: disnake.User):
+        """Get a users avatar"""
+
+        embed = disnake.Embed(
+            color=SUCCESS,
+            title=f"{user.mention}'s avatar"
+        )
+
+        embed.set_image(user.display_avatar)
+        await inter.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
