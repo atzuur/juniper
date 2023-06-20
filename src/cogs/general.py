07177088ac53b760a0bc33c73f3090f5da4dddc0
june <136140456+june550@users.noreply.github.com>
2023-06-19 23:51:23 -0400

    Update general.py

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -17,7 +17,13 @@ class GeneralCommands(commands.Cog):
 
     @commands.slash_command()
     async def avatar(inter: disnake.AppCmdInter, user: disnake.User):
-        """Get a users avatar"""
+        """
+        Get a users avatar
+
+        Parameters
+        ----------
+        user: Mention a user or enter their ID
+        """
 
         embed = disnake.Embed(
             color=SUCCESS,
@@ -26,6 +32,65 @@ class GeneralCommands(commands.Cog):
 
         embed.set_image(user.display_avatar)
         await inter.send(embed=embed)
+
+
+    @commands.slash_command()
+    async def member_info(inter: disnake.AppCmdInter, member: disnake.Member):
+        """
+        Get info about a member
+
+        Parameters
+        ----------
+        member: Mention a member or enter their ID
+        """
+
+        embed = disnake.Embed(color=SUCCESS)
+
+        embed.set_author(name=member, icon_url=member.display_avatar)
+        embed.set_thumbnail(member.display_avatar)
+
+        embed.add_field(
+            name="ID",
+            value=member.id,
+            inline=False
+        )
+
+        creation_date = f"<t:{int(member.created_at.timestamp())}:R>"
+        embed.add_field(
+            name="Created",
+            value=creation_date,
+            inline=False
+        )
+
+        join_date = f"<t:{int(member.joined_at.timestamp())}:R>"
+        embed.add_field(
+            name="Joined",
+            value=join_date,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Status",
+            value=member.status.name.capitalize(),
+            inline=False
+        )
+
+        roles = member.roles[1:] # exclude @everyone
+        
+        if len(roles) > 0:
+            embed.add_field(
+                name=f"Roles [{len(roles)}]",
+                value=", ".join(role.mention for role in roles),
+                inline=False
+            )
+
+        
+        await inter.send(embed=embed)  
+
+    @member_info.error
+    async def info_error(inter: disnake.AppCmdInter, error):
+        if isinstance(error, commands.BadArgument):
+            await inter.send("Sprry, couldn't find that member", ephemeral=True)
 
 
 def setup(bot: commands.Bot):
