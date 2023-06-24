39cfba04e26642ea0e072979a605c172946ef1a3
june <136140456+june550@users.noreply.github.com>
2023-06-23 20:14:20 -0400

    Update sudo.py

--- src/cogs/sudo.py
+++ src/cogs/sudo.py
@@ -3,41 +3,53 @@ from disnake.ext import commands
 
 import config as cfg
 
-# sudo commands for the bot owner
+# event logging
 
-class Sudo(commands.Cog):
+class Logging(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    @commands.is_owner()
-    @commands.slash_command()
-    async def shutdown(self, inter: disnake.AppCmdInter):
-        """Shutdown the bot (owner only)."""
-
+    
+        
+    @commands.Cog.listener()
+    async def on_slash_command(self, inter: disnake.AppCmdInter):
+        
+        if inter.command_failed:
+            return
+        
+        author = inter.author.mention
+        command = inter.data.name
+        
         embed = disnake.Embed(
             color=cfg.SUCCESS,
-            title="Shutting Down",
-            description="Shutting down.. See you later! :wave:"
+            title=f"Command Executed",
+            description=f"{author} executed **{command}**."
         )
-
-        await inter.send(embed=embed)
-        await self.bot.close()
-
-
-    @shutdown.error
-    async def shutdown_error(self, inter: disnake.AppCmdInter, error):
-        if isinstance(error, commands.NotOwner):
-
-            embed = disnake.Embed(
-                color=cfg.ERROR,
-                title="Error",
-                description=error
-            )
-
-            await inter.send(embed=embed, ephemeral=True)
-
-
+        
+        embed.set_author(name=inter.author, icon_url=inter.author.display_avatar)
+        
+        logs_channel = self.bot.get_channel(cfg.LOGS_CHANNEL)
+        await logs_channel.send(embed=embed)
+    
+        
+    @commands.Cog.listener()
+    async def on_slash_command_error(self, inter: disnake.AppCmdInter, error: commands.CommandError):
+        
+        author = inter.author.mention
+        command = inter.data.name
+        
+        embed = disnake.Embed(
+            color=cfg.ERROR,
+            title=f"Command Failed",
+            description=f"{author} failed to execute **{command}**.\n Error: {error}"
+        )
+        
+        embed.set_author(name=inter.author, icon_url=inter.author.display_avatar)
+        
+        logs_channel = self.bot.get_channel(cfg.LOGS_CHANNEL)
+        await logs_channel.send(embed=embed)
+        
+        
 def setup(bot: commands.Bot):
-    bot.add_cog(Sudo(bot))
+    bot.add_cog(Logging(bot))
     print(f"{__name__} is ready.")
