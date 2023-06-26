4ed4c848dc413a53abd2d6630f0517b0d1092944
june <136140456+june550@users.noreply.github.com>
2023-06-25 20:59:35 -0400

    Update sudo.py

--- src/cogs/sudo.py
+++ src/cogs/sudo.py
@@ -13,7 +13,7 @@ class Sudo(commands.Cog):
     @commands.is_owner()
     @commands.slash_command()
     async def shutdown(self, inter: disnake.AppCmdInter):
-        """Shutdown the bot (owner only)"""
+        """Shuts down the bot (owner only)"""
 
         embed = disnake.Embed(
             color=cfg.SUCCESS,
